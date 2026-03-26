50b53d77ab5047075b6da18aa1b51b3581ffba45
eberzina <berzina3633@gmail.com>
2026-03-26 10:34:26 +0200

    Add new components and style main heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
+import {Error} from './Error'
 import {InputSearch} from '../components/InputSearch';
+import { CurrentWeather } from '../components/CurrentWeather';
+import {DailyForecast} from '../components/DailyForecast';
 
 export function Home() {
 
   return (
     <>
-      <h1>How's the sky looking today?</h1>
-      <InputSearch />
+      <section>
+        <h1 className='text-5xl font-display text-center leading-14 tracking-tight py-10'>How's the sky looking today?</h1>
+        <InputSearch />
+        <CurrentWeather />
+        <DailyForecast />
+      </section>
+      {/* <Error /> */}
     </>
   )
 }
