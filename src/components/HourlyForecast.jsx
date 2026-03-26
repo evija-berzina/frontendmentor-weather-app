d8b3abf13ff537aec56431c34cfd1741b796f389
eberzina <berzina3633@gmail.com>
2026-03-26 14:29:49 +0200

    Create and style HourlyForecast component

---
+++ src/components/HourlyForecast.jsx
@@ -0,0 +1,44 @@
+import IconDropdown from '../assets/images/icon-dropdown.svg';
+import IconCloudy from '../assets/images/icon-overcast.webp';
+
+export function HourlyForecast() {
+  return (
+    <div className='flex flex-col justify-center items-center gap-2 bg-[hsl(var(--neutral-800))] rounded-2xl p-4'>
+      <div className='flex flex-row justify-between items-center w-full'>
+        <h2 className='font-normal'>Hourly forecast</h2>
+        <button className='flex flex-row justify-center items-center gap-2 px-4 py-1.5 bg-[hsl(var(--neutral-600))] rounded-sm text-xs font-light'>
+          Tuesday
+          <img className='w-2 h-2' src={IconDropdown} alt="" />
+        </button>
+      </div>
+      <div className='flex flex-row justify-between items-center w-full bg-[hsl(var(--neutral-700))] rounded-lg border border-[hsl(var(--neutral-600))] px-4 py-2'>
+        <div className='flex flex-row justify-center items-center gap-2'>
+          <img className='w-10' src={IconCloudy} alt="" />
+          <p>3 PM</p>
+        </div>
+        <p className='text-sm'>68°</p>
+      </div>
+      <div className='flex flex-row justify-between items-center w-full bg-[hsl(var(--neutral-700))] rounded-lg border border-[hsl(var(--neutral-600))] px-4 py-2'>
+        <div className='flex flex-row justify-center items-center gap-2'>
+          <img className='w-10' src={IconCloudy} alt="" />
+          <p>3 PM</p>
+        </div>
+        <p className='text-sm'>68°</p>
+      </div>
+      <div className='flex flex-row justify-between items-center w-full bg-[hsl(var(--neutral-700))] rounded-lg border border-[hsl(var(--neutral-600))] px-4 py-2'>
+        <div className='flex flex-row justify-center items-center gap-2'>
+          <img className='w-10' src={IconCloudy} alt="" />
+          <p>3 PM</p>
+        </div>
+        <p className='text-sm'>68°</p>
+      </div>
+      <div className='flex flex-row justify-between items-center w-full bg-[hsl(var(--neutral-700))] rounded-lg border border-[hsl(var(--neutral-600))] px-4 py-2'>
+        <div className='flex flex-row justify-center items-center gap-2'>
+          <img className='w-10' src={IconCloudy} alt="" />
+          <p>3 PM</p>
+        </div>
+        <p className='text-sm'>68°</p>
+      </div>
+    </div>
+  )
+}
