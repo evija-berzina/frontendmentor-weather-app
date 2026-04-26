49082908a984932dfb14182627d042b0c16daf46
eberzina <berzina3633@gmail.com>
2026-04-26 17:03:13 +0300

    Add hover effect to hourly forecast day section and try to add loading state look

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -34,15 +34,14 @@ export function HourlyForecast({data, getWeatherIcon, showUnits, setShowUnits}) 
   function hourlyForecastDays() {
     return(
       <>
-        <div className='bg-[hsl(var(--neutral-800))] border border-[hsl(var(--neutral-600))] rounded-md text-xs p-2 w-40 absolute top-12 right-4 z-10'>
+        <div className='flex flex-col gap-1 bg-[hsl(var(--neutral-800))] border border-[hsl(var(--neutral-600))] rounded-md text-xs p-2 w-40 absolute top-12 right-4 z-10'>
           {data.daily.map((days) => (
-            <label htmlFor={days.day} key={days.day} className='flex flex-row justify-between items-center p-2 w-full'>
+            <label htmlFor={days.day} key={days.day} className={`flex flex-row justify-between items-center p-2 w-full hover:bg-[hsl(var(--neutral-700))] hover:rounded-md cursor-pointer ${days.day === selectedDay ? 'bg-[hsl(var(--neutral-700))] rounded-md' : ''}`}>
               <input
                 type="radio"
                 id={days.day}
                 name="daily"
                 value={days.day}
-                // checked={}
                 onChange={() => setSelectedDay(days.day)}
                 className="hidden" />
               {dayjs(days.day).format('dddd')}
@@ -58,34 +57,47 @@ export function HourlyForecast({data, getWeatherIcon, showUnits, setShowUnits}) 
       <div className='flex flex-row justify-between items-center w-full pe-4'>
         <h2 className='font-normal'>Hourly forecast</h2>
         <button
-          className='flex flex-row justify-center items-center gap-2 px-4 py-1.5 bg-[hsl(var(--neutral-600))] rounded-sm text-xs font-light'
+          className='flex flex-row justify-center items-center gap-2 px-4 py-1.5 bg-[hsl(var(--neutral-600))] rounded-sm text-xs font-light cursor-pointer'
           onClick={() => setShowUnits(!showUnits)}
         >
-          {dayjs(data.daily?.[0]?.day).format('dddd') || '-'}
+          {selectedDay === null
+            ? data.daily?.[0]?.day
+              ? dayjs(data.daily[0].day).format('dddd')
+              : '-'
+            : dayjs(selectedDay).format('dddd')
+          }
           <img className='w-2 h-2' src={IconDropdown} alt="" />
         </button>
       </div>
       <div>{showUnits ? hourlyForecastDays() : null}</div>
       <div className='flex flex-col w-full gap-4 overflow-y-auto h-144 pe-4'>
-        {selectedDay === null
-          ? hourTime2.map((hour) => (
-            <div key={hour.time} className='flex flex-row justify-between items-center w-full bg-[hsl(var(--neutral-700))] rounded-lg border border-[hsl(var(--neutral-600))] px-4 py-2'>
+        {data.hourly.length === 0
+          ? <div className='flex flex-row justify-between items-center w-full bg-[hsl(var(--neutral-700))] rounded-lg border border-[hsl(var(--neutral-600))] px-4 py-2'>
               <div className='flex flex-row justify-center items-center gap-2'>
-                <img className='w-10' src={getWeatherIcon(hour.weatherCode)} alt="" />
-                <p>{hour.time}</p>
+                <img className='w-10' src='' alt="" />
+                <p></p>
               </div>
-              <p className='text-sm'>{hour.temperature}°</p>
+              <p className='text-sm'></p>
             </div>
-          ))
-          : dayTime.map((hour) => (
-            <div key={hour.time} className='flex flex-row justify-between items-center w-full bg-[hsl(var(--neutral-700))] rounded-lg border border-[hsl(var(--neutral-600))] px-4 py-2'>
-              <div className='flex flex-row justify-center items-center gap-2'>
-                <img className='w-10' src={getWeatherIcon(hour.weatherCode)} alt="" />
-                <p>{hour.time}</p>
+          : selectedDay === null
+            ? hourTime2.map((hour) => (
+              <div key={hour.time} className='flex flex-row justify-between items-center w-full bg-[hsl(var(--neutral-700))] rounded-lg border border-[hsl(var(--neutral-600))] px-4 py-2'>
+                <div className='flex flex-row justify-center items-center gap-2'>
+                  <img className='w-10' src={getWeatherIcon(hour.weatherCode)} alt="" />
+                  <p>{hour.time}</p>
+                </div>
+                <p className='text-sm'>{hour.temperature}°</p>
               </div>
-              <p className='text-sm'>{hour.temperature}°</p>
-            </div>
-          ))
+            ))
+            : dayTime.map((hour) => (
+              <div key={hour.time} className='flex flex-row justify-between items-center w-full bg-[hsl(var(--neutral-700))] rounded-lg border border-[hsl(var(--neutral-600))] px-4 py-2'>
+                <div className='flex flex-row justify-center items-center gap-2'>
+                  <img className='w-10' src={getWeatherIcon(hour.weatherCode)} alt="" />
+                  <p>{hour.time}</p>
+                </div>
+                <p className='text-sm'>{hour.temperature}°</p>
+              </div>
+            ))
         }
       </div>
     </div> 
