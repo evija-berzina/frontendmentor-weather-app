6c8987f214ee37cc2c5535cd6e7882f287f3aa8c
eberzina <berzina3633@gmail.com>
2026-04-30 18:23:45 +0300

    Fix unit dropdown positioning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,11 @@ export function Header({unit, setUnit, showUnits, setShowUnits}) {
           <button onClick={() => setShowUnits(!showUnits)} className='flex flex-row justify-center items-center gap-2 px-5 py-3 bg-[hsl(var(--neutral-800))] rounded-md text-sm font-light cursor-pointer hover:bg-[hsl(var(--neutral-700))] transform transition-colors duration-300 ease-in-out'>
             <img className='w-4 h-4' src={IconUnits} alt="" />
             Units
-            <img className='w-3 h-3' src={IconDropdown} alt="" />
+            <img
+              src={IconDropdown}
+              className={`w-3 h-3 transform transition-transform duration-300 ease-in-out ${showUnits ? "rotate-180" : "rotate-0"}`}
+              alt=""
+            />
           </button>
           {showUnits && <UnitsPanel unit={unit} setUnit={setUnit} />}
         </nav>
