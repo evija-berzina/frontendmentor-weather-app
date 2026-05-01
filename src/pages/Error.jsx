3d5a52f979f5c9301c58af272429edab0b0855ee
eberzina <berzina3633@gmail.com>
2026-05-01 15:29:32 +0300

    Update layout for Error component

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -15,11 +15,11 @@ export function Error({reload}) {
     <div className='flex flex-col justify-center items-center text-center mt-20'>
       <img src={IconError} alt="" className='w-10 h-10' />
       <h1 className='text-5xl font-display text-center leading-14 tracking-tight py-4'>Something went wrong</h1>
-      <p className='px-145 text-[hsl(var(--neutral-200))] mb-6'>
+      <p className='text-[hsl(var(--neutral-200))] mb-6'>
         We couldn't connect to the server (API error). Please try again in a few moments.
       </p>
       <button
-        className='flex flex-row gap-2 rounded-md bg-[hsl(var(--neutral-600))] py-2 px-6 text-sm cursor-pointer hover:border hover:border-[hsl(var(--neutral-300))] transition-colors duration-300 ease-in-out'
+        className='flex flex-row gap-2 rounded-md bg-[hsl(var(--neutral-600))] py-2 px-6 text-sm cursor-pointer hover:shadow-[0_0_3px_hsl(var(--neutral-300))] transition-colors duration-300 ease-in-out'
         onClick={() => {
           handleReload();
           reload();
